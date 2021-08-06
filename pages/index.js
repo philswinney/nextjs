77f01f33449f24cfe7df68f2a7f5edb09988516d
philswinney <phil@philswinney.com>
2021-08-06 09:46:30 -0300

    format updated

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
         a href = "https://nextjs.org/docs"
         className = { styles.card } >
         <
-        h3 > Documentation & rarr; < /h3> <
+        h3 > Documentation &rarr;< /h3> <
         p > Find in -depth information about Next.js features and API. < /p> < /
         a >
 
@@ -40,7 +40,7 @@ export default function Home() {
         a href = "https://nextjs.org/learn"
         className = { styles.card } >
         <
-        h3 > Learn & rarr; < /h3> <
+        h3 > Learn &rarr;< /h3> <
         p > Learn about Next.js in an interactive course with quizzes! < /p> < /
         a >
 
@@ -48,7 +48,7 @@ export default function Home() {
         a href = "https://github.com/vercel/next.js/tree/master/examples"
         className = { styles.card } >
         <
-        h3 > Examples & rarr; < /h3> <
+        h3 > Examples &rarr;< /h3> <
         p > Discover and deploy boilerplate example Next.js projects. < /p> < /
         a >
 
@@ -56,7 +56,7 @@ export default function Home() {
         a href = "https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         className = { styles.card } >
         <
-        h3 > Deploy & rarr; < /h3> <
+        h3 > Deploy &rarr;< /h3> <
         p >
         Instantly deploy your Next.js site to a public URL with Vercel. <
         /p> < /
